7f8540e92dc1aa3f8c4c1897737b43272bccebb8
琪峰 陈 <635903897@qq.com>
2019-11-25 15:45:55 +0800

    HMSystemDbContext类添加了上下文代码

--- 4.2.0/src/HMSystem.EntityFrameworkCore/EntityFrameworkCore/HMSystemDbContext.cs
+++ 4.2.0/src/HMSystem.EntityFrameworkCore/EntityFrameworkCore/HMSystemDbContext.cs
@@ -6,7 +6,11 @@ namespace HMSystem.EntityFrameworkCore
     public class HMSystemDbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
-
+        public DbSet<ServicePersonalBase> Persons { get; set; }
+        public DbSet<ServicePersonalCertificates> Certificates { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<WorkRecord> WorkRecords { get; set; }
         public HMSystemDbContext(DbContextOptions<HMSystemDbContext> options) 
             : base(options)
         {
